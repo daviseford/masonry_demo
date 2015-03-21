0d2ec4cd429aa3bdd7ef035f9139e4d5c445a3d2
Davis Ford <davis.e.ford@gmail.com>
2015-03-21 01:22:40 -0400

    initial commit

---
+++ js/davis.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+$(document).ready(function(){
+
+
+var container = document.querySelector('#container');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 200,
+  itemSelector: '.item'
+});
+//
+//var msnry1 = new Masonry( '#container', {
+//  // opti1ons
+//});
+//
+//var $container = $('#container');
+//// initialize
+//$container.masonry({
+//  columnWidth: 200,
+//  itemSelector: '.item'
+//});
+});
